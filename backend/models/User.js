828113989bcae30746ef9521b5288e5a94c6ec8c
sunadi amarasinghe <sunadiamarasinghe@gmail.com>
2025-08-14 19:47:18 +0530

    Improved password hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); // Add this at top
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,7 +24,18 @@ const userSchema = new mongoose.Schema({
   resetPasswordToken: String,
   resetPasswordExpires: Date
 }, { timestamps: true });
-userSchema.methods.matchPassword = async function (enteredPassword) {
+
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
